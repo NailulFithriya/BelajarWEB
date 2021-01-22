c7059bfa1dccf7e519f7469823b2f1adfafb0e9e
NailulFithriya <nailulfithriya2510@gmail.com>
2021-01-22 21:50:54 +0700

    create new folder

---
+++ src/Latihan1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>No</th>
+      <th>Nama</th>
+      <th>NPM</th>
+      <th>Jurusan</th>
+      <th>Gambar</th>
+      <th>Aksi</th>
+    </tr>
+    <td>1</td>
+    <td>1</td>
+    <td>Nailul Fithriya</td>
+    <td>1908107010023</td>
+    <td>Informatika</td>
+    <td><img src="img/OIP.jpg" alt=""></td>
+    <td>
+      <a href="">ubah</a> | <a href="">hapus</a>
+    </td>
+  </table>
+</body>
+
+</html>
